a45db8cf8cb02f7bb17816bd3d483da33c9236ac
Vova2580 <109892815+Vova2580@users.noreply.github.com>
2022-12-15 23:31:30 +0200

    Готова задача 1

---
+++ js/01-gallery.js
@@ -0,0 +1,64 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryItem = createGalleryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItem);
+
+galleryContainer.addEventListener("click", onGalleruContainerClick);
+
+function createGalleryItem(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => { 
+            return `<div class="gallery__item">
+        <a
+        class="gallery__link"
+        href = "${original}"
+        >
+        <img
+            class="gallery__image"
+            src = "${preview}"
+            data-source = "${original}"
+            alt = "${description}"
+        />
+        </a>
+        </div>`
+    }).join('');
+};
+
+function onGalleruContainerClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const bigImageUrl = event.target.getAttribute("data-source");
+
+    const instance = basicLightbox.create(`<img src=${bigImageUrl}>`);
+    instance.show();
+
+    window.addEventListener("keydown", onInstancePress);
+    window.addEventListener("click", onInstancePress);
+
+    function onInstancePress(event) {
+            
+        if (event.key === "Escape") {
+            instance.close();
+        }
+    }
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
