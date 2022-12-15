422db74e4658663cbd6d4a095c2cda438400c817
Vova2580 <109892815+Vova2580@users.noreply.github.com>
2022-12-15 23:45:16 +0200

    Початок Завдання 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const items = createGalleryItem(galleryItems);
+galleryList.insertAdjacentHTML("beforeend",items);
+
+galleryList.addEventListener("click", onGalleryListClick);
+
+
+function createGalleryItem(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+        }).join('');
+    
+}
+
+
+
+function onGalleryListClick(evt) {
+    evt.preventDefault();
+}
+
